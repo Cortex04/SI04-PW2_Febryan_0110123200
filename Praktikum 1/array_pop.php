849e8936d8a1c1993a4e37d9b09f36c06c730103
Cortex04 <107186310+Cortex04@users.noreply.github.com>
2024-03-03 23:05:46 +0700

    Upload praktikum 1

---
+++ Praktikum 1/array_pop.php
@@ -0,0 +1,7 @@
+<?php
+$tims = ["Erwin", "heru", "ali", "Zaki"];
+
+array_pop( array: $tims );
+foreach ( $tims as $person ) {
+    echo $person . "</br>";
+}
---
+++ Praktikum 1/variable_user.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Febryan Ahmad Tri Setyo";
+$umur = 20;
+$berat = 50.5;
+
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+
+echo "<br/>Hello $nama Apakabar";
+?>
